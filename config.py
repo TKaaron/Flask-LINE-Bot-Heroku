91fe92481912da3cfebc5c72b72e94424b1dfee5
TKaaron <74527740+TKaaron@users.noreply.github.com>
2021-01-02 23:14:41 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+#line-bot
+channel_access_token = 'cly7Lyd4Z3TB9/fe/Q6Tw0WdjaI97pUxEu8Psvakl3hOgUHpDb+Tdu6XbAkuMqfW7Yn75XOAchAkcVXQBFM/V0XW+jfEavlSHVdcHZiq4QkiYiUfrrU+U+PCjTd+aamsyyJSzjy5Lv+c1eGeE2fu6QdB04t89/1O/w1cDnyilFU=' 
+channel_secret = 'ed3e116a06c707a2f8fb182078f33cbb'
